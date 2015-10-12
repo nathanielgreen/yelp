47fe794c3c5a144464d9710d785d8cf901f5f68c
philip <philip.clarke@redhood.co.uk>
2015-10-12 13:06:22 +0100

    created and raked new databases

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
-feature 'restuarants' do
-  context 'no restuarants have been added' do
-    scenario 'should display a prompt to add a resturant' do
+feature 'restaurants' do
+  context 'no restaurants have been added' do
+    scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurants have been added' do
+    before
+      Restaurant.create(name: 'The Ox')
+  end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('The Ox')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+
+
 end
