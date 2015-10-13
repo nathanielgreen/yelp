781fc741ac57ca38c13429895eca18523c404583
philip <philip.clarke@redhood.co.uk>
2015-10-13 10:22:19 +0100

    create new controller for restaurant params

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,4 +8,13 @@ class RestaurantsController < ApplicationController
 
   end
 
+  def create
+    Restaurant.create(params[restaurant_params])
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
