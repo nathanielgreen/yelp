17965d3b061dd957ee1c6ac7599e11de90065ca6
philip <philip.clarke@redhood.co.uk>
2015-10-13 09:24:59 +0100

    new.html.haml updated with new form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,9 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+
+  end
+
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -11,15 +11,26 @@ feature 'restaurants' do
 
   context 'restaurants have been added' do
     before do
-      Restaurant.create(name: 'The Ox')
+      Restaurant.create(name: 'KFC')
     end
 
     scenario 'display restaurants' do
       visit '/restaurants'
-      expect(page).to have_content('The Ox')
+      expect(page).to have_content('KFC')
       expect(page).not_to have_content('No restaurants yet')
     end
   end
+
+  context 'creating restaurants' do
+  scenario 'prompts user to fill out a form, then displays the new restaurant' do
+    visit '/restaurants'
+    click_link 'Add a restaurant'
+    fill_in 'Name', with: 'KFC'
+    click_button 'Create Restaurant'
+    expect(page).to have_content 'KFC'
+    expect(current_path).to eq '/restaurants'
+  end
+end
 
 
 end
